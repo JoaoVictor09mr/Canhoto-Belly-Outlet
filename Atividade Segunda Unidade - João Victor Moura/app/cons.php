c3abf7ba577064b70e259356f0845844910f8136
JoaoVictor09mr <202413600072@ifba.edu.br>
2026-09-14 10:03:31 -0300

    Delete Atividade Segunda Unidade - João Victor Moura directory

--- Atividade Segunda Unidade - João Victor Moura/app/cons.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$server = "localhost";
-$user = "root";
-$password = "";
-$db = "loja_canhoto_belly";
-?>
--- Atividade Segunda Unidade - João Victor Moura/finalizar_carrinho.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Mantido como página alternativa de finalização.
-// O processamento da venda é feito por salvar_venda.php.
-header('Location: ver_carrinho.php');
-exit;
-?>
